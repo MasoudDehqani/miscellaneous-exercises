48a540c05254c9056bd5a5ba5d6f021052e06c78
Masoud <masoud67.md@gmail.com>
2021-02-12 16:33:54 +0330

    repeater function exercise improved

--- repeater-function/repeater.js
+++ repeater-function/repeater.js
@@ -1,15 +1,32 @@
 let array = [1, [3, 8], 57, 6, {n: 2}]
 
-function repeatBy(arr, repeat) {
-	let repeatedArray = []
-	for (let i = 0; i < arr.length; i++) {
-		let repeated = 0
-		while (repeated < repeat) {
-			repeatedArray.push(arr[i])
-			repeated++
+// function repeatBy(arr, repeat) {
+// 	let repeatedArray = []
+// 	for (let i = 0; i < arr.length; i++) {
+// 		let repeated = 0
+// 		while (repeated < repeat) {
+// 			repeatedArray.push(arr[i])
+// 			repeated++
+// 		}
+// 	}
+// 	return repeatedArray
+// }
+
+// console.log(repeatBy(array, 2))
+
+
+function repeatBy(arr) {
+	return function r2(num) {
+		let repeatedArray = []
+		for (let i = 0; i < arr.length; i++) {
+			let repeated = 0
+			while (repeated < num) {
+				repeatedArray.push(arr[i])
+				repeated++
+			}
 		}
+		return repeatedArray
 	}
-	return repeatedArray
 }
 
-console.log(repeatBy(array, 2))
+console.log(repeatBy(array)(2))
